b483c74d2ffa823f5d10e37dfb9dddb22f974096
ronilpatil <ronilpatil2000@gmail.com>
2024-05-01 16:47:15 +0530

    implement train_model & visualization stage

---
+++ src/models/train_model.py
@@ -0,0 +1,107 @@
+import pathlib
+import yaml
+import joblib
+import mlflow
+import typing
+import numpy as np
+import pandas as pd
+from mlflow.sklearn import log_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.base import BaseEstimator
+from src.logger import infologger
+
+infologger.info('*** Executing: train_model.py ***')
+# writing import after infologger to log the info precisely 
+from src.visualization import visualize
+from src.data.make_dataset import load_data
+
+def train_model(training_feat: np.ndarray, y_true: pd.Series, n_estimators: int, criterion: str, max_depth: int, min_samples_split: int, min_samples_leaf: int, random_state: int, yaml_file_obj: typing.IO) -> dict :
+     try : 
+          model = RandomForestClassifier(n_estimators = n_estimators, criterion = criterion, max_depth = max_depth, min_samples_split = min_samples_split,
+                                         min_samples_leaf = min_samples_leaf, random_state = random_state)
+          model.fit(training_feat, y_true)
+     except Exception as e :
+          infologger.info(f'unable to fit RFC model [check train_model()]. exc: {e}')
+     else :
+          infologger.info(f'trained {type(model).__name__} model')
+          y_pred = model.predict(training_feat)
+          y_pred_prob = model.predict_proba(training_feat)
+          accuracy = round(metrics.balanced_accuracy_score(y_true, y_pred), 5)
+          precision = round(metrics.precision_score(y_true, y_pred, zero_division = 1, average = 'macro'), 5)
+          recall = round(metrics.recall_score(y_true, y_pred, average = 'macro'), 5)
+          roc_score = round(metrics.roc_auc_score(y_true, y_pred_prob, average = 'macro', multi_class = 'ovr'), 5)
+
+          return {'model': model, 
+                  'y_pred': y_pred, 
+                  'params': {"n_estimator": n_estimators, "criterion": criterion,
+                             "max_depth": max_depth, "seed": random_state},
+                  'metrics': {"accuracy": accuracy, "precision": precision, "recall": recall, 
+                              "roc_score": roc_score}}
+
+def save_model(model: BaseEstimator, model_dir: str, model_name: str = 'model') -> None : 
+     try : 
+          joblib.dump(model, f'{model_dir}/{model_name}.joblib')
+     except Exception as e : 
+          infologger.info(f'unable to save the model [check save_model(). exc: {e}')
+     else :
+          infologger.info(f'model saved successfully [loc: {model_dir}]')
+
+def main() -> None : 
+     curr_path = pathlib.Path(__file__) 
+     home_dir = curr_path.parent.parent.parent.as_posix()
+     params_loc = f'{home_dir}/params.yaml'
+     plots_dir = f'{home_dir}/figures/training'
+     try : 
+          params = yaml.safe_load(open(params_loc, encoding = 'utf8'))
+     except Exception as e :
+          infologger.info(f'unable to load params.yaml [check main()]. exc: {e}')
+     else : 
+          parameters = params['train_model']
+          TARGET = params['base']['target']
+
+          train_data = f"{home_dir}{params['build_features']['processed_data']}/processed_train.csv"
+          model_dir = f"{home_dir}{parameters['model_dir']}"
+          pathlib.Path(model_dir).mkdir(parents = True, exist_ok = True)
+          
+          data = load_data(train_data)
+          X_train = data.drop(columns = [TARGET]).values
+          Y = data[TARGET]
+
+          details = train_model(X_train, Y, parameters['n_estimators'], parameters['criterion'], parameters['max_depth'], min_samples_leaf = parameters['min_samples_leaf'],
+                                 min_samples_split = parameters['min_samples_split'], random_state = parameters['random_state'], yaml_file_obj = params)
+
+          filename = visualize.conf_matrix(Y, details['y_pred'], labels = details['model'].classes_, path = plots_dir, params_obj = params)
+
+          mlflow_config = params['mlflow_config']
+          remote_server_uri = mlflow_config['remote_server_uri']
+          exp_name = mlflow_config['trainingExpName']
+
+          mlflow.set_tracking_uri(remote_server_uri)
+          mlflow.set_experiment(experiment_name = exp_name)
+          # adding experiment description
+          experiment_description = ('training RFC model. Obj is to predict the wine quality based on various physicochemical features') 
+ 
+          mlflow.set_experiment_tag("mlflow.note.content", experiment_description)
+          
+          # runs description
+          with mlflow.start_run(description = 'RFC - ronil') : 
+               # logging the prarmeters
+               mlflow.log_params({"n_estimator": details['params']['n_estimator'], "criterion": details['params']['criterion'], 
+                                  "max_depth": details['params']['max_depth'], "seed": details['params']['seed']})
+               # logging metrics
+               mlflow.log_metrics({"accuracy": details['metrics']['accuracy'], "precision": details['metrics']['precision'], 
+                                   "recall": details['metrics']['recall'], "roc_score": details['metrics']['roc_score']})
+               # loagging the current run's model
+               log_model(details['model'], "model")
+               # logging confusion matrix img
+               mlflow.log_artifact(filename, 'confusion_matrix')
+               # setting tags to each run
+               mlflow.set_tags({'project_name': 'wine-quality', 'project_quarter': 'Q1-2024', 'ml_model' : 'RFC'})
+
+          save_model(details['model'], model_dir)
+
+if __name__ == '__main__' : 
+     infologger.info('train_model.py as __main__')
+     main()
+     
---
+++ src/visualization/visualize.py
@@ -0,0 +1,79 @@
+import pathlib
+import yaml
+import typing
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import joblib
+from datetime import datetime
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from src.logger import infologger
+from sklearn.base import BaseEstimator
+
+infologger.info('*** Executing: visualize.py ***')
+from src.data.make_dataset import load_data
+
+def load_model(model_dir: str) -> BaseEstimator :
+     try : 
+          model = joblib.load(model_dir)
+     except Exception as e : 
+          infologger.info(f'unable to load the model from {model_dir} [check load_model()]. exc: {e}')
+     else : 
+          infologger.info(f'model loaded successfully [loc: {model_dir}]')
+          return model
+
+def roc_curve() -> None : 
+     pass
+
+def conf_matrix(y_test: pd.Series, y_pred: pd.Series, labels: np.ndarray, path: pathlib.Path, params_obj: typing.IO) -> str : 
+     try : 
+          curr_time = datetime.now().strftime('%d%m%y-%H%M%S')
+          dir_path = pathlib.Path(f'{path}/cMatrix')
+          dir_path.mkdir(parents = True, exist_ok = True)
+     except Exception as e : 
+          infologger.info(f'something wrong with directories [check conf_metrix()]. exc: {e}')
+     else :
+          infologger.info('directories are all set!')
+          try :
+               cm = confusion_matrix(y_test, y_pred, labels = labels)
+               disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = labels)
+          except Exception as e : 
+               infologger.info(f'unable to plot the confusion metrix [check conf_metrix()]. exc: {e}')
+          else :
+               disp.plot(cmap = plt.cm.Blues)
+               plt.title('Confusion Matrix')
+               plt.xlabel('Predicted Label')
+               plt.ylabel('True Label')
+               filename = f'{dir_path.as_posix()}/{curr_time}.png'
+               plt.savefig(filename)
+               plt.close()
+               infologger.info(f'confusion metrix saved at [{dir_path}]')
+               return filename
+          
+def main() -> None :
+     curr_dir = pathlib.Path(__file__)
+     home_dir = curr_dir.parent.parent.parent.as_posix()
+     dir_path = f'{home_dir}/figures'
+     dir_path.mkdir(parents = True, exist_ok = True)
+     params = yaml.safe_load(open(f'{home_dir}/params.yaml'))
+     data_dir = f"{home_dir}{params['build_features']['processed_data']}/processed_test.csv"
+     model_dir = f'{home_dir}{params["train_model"]["model_dir"]}/model.joblib'
+     
+     TARGET = params['base']['target']
+
+     test_data = load_data(data_dir)
+     x_test = test_data.drop(columns = [TARGET]).values
+     y_test = test_data[TARGET]
+     
+     model = load_model(model_dir)
+     labels = model.classes_
+     try : 
+          y_pred = model.predict(x_test)     # return class
+     except Exception as e : 
+          infologger.info(f'unable to make prediction [check main()]. exc: {e}')
+     else :
+          conf_matrix(y_test, y_pred, labels, dir_path, yaml_file_obj = params)
+               
+if __name__ == '__main__' :
+     infologger.info('visualize.py as __main__')
+     main()
